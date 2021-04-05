98675520a2114a340940998a07e6b5a638fc4eea
proyecto42jmma <81994344+proyecto42jmma@users.noreply.github.com>
2021-04-05 16:49:49 -0500

    Create tipos.js

---
+++ js/tipos.js
@@ -0,0 +1,25 @@
+/**
+ * @typedef {Objeto} Rol
+ * @property {cadena} descripción
+ */
+
+/**
+ * @typedef {Objeto} Alumno
+ * @property {cadena} nombre
+ */
+
+/**
+ * @typedef {Objeto} Usuario
+ * @property {string[]} rolIds
+ * @property {string} alumnoId
+ */
+
+// @ts-sin control
+/**
+ * @typedef {Objeto} Mensaje
+ * @property {string} usuarioId
+ * @property {string} texto
+ * @property {firebase.firestore.Timestamp} marca de tiempo
+ */
+
+exportación const __tipos = 0;
