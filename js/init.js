b48cd3e27b757cc37bd75e5b1b23b25645040063
proyecto42jmma <81994344+proyecto42jmma@users.noreply.github.com>
2021-04-05 16:34:36 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,8 @@
+apiKey: "AIzaSyCuPIHa13xd4ZjCIaPqJzcyqSbTnd2hu8o",
+    authDomain: "proyecto42jma.firebaseapp.com",
+    projectId: "proyecto42jma",
+    storageBucket: "proyecto42jma.appspot.com",
+    messagingSenderId: "699283679255",
+    appId: "1:699283679255:web:cb79877fe12159a78c9452",
+    measurementId: "G-N8QVXZB0NN"
+  };
