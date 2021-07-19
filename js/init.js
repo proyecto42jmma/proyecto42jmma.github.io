e56773b10e87ce2807181700bae785aa86c3ab3b
proyecto42jmma <81994344+proyecto42jmma@users.noreply.github.com>
2021-07-19 14:11:50 -0500

    Delete js directory

--- js/init.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp({
-    apiKey: "AIzaSyCuPIHa13xd4ZjCIaPqJzcyqSbTnd2hu8o",
-    authDomain: "proyecto42jma.firebaseapp.com",
-    projectId: "proyecto42jma",
-    storageBucket: "proyecto42jma.appspot.com",
-    messagingSenderId: "699283679255",
-    appId: "1:699283679255:web:cb79877fe12159a78c9452",
-    measurementId: "G-N8QVXZB0NN"
-  });
